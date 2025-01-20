02b68bd697960b2a5ff41843bfcb6291e74428fb
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2025-01-19 22:30:22 -0300

    add project url

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 
   :description "A Leiningen template to validate library compatibility with GraalVM native image generation."
 
-  :url "http://example.com/FIXME"
+  :url "https://github.com/macielti/simple-graalvm"
 
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
